09962a52f26110bbe6c2f0b7a5df1401f4393c72
Konrad Krueger <KonKrueger@gmail.com>
2021-08-06 12:24:03 +0200

    first test for server.decodeplayercolor

--- human-dont-get-mad/src/testing/server/ServerControllerTest.java
+++ human-dont-get-mad/src/testing/server/ServerControllerTest.java
@@ -1,5 +1,28 @@
 package testing.server;
 
-public class ServerControllerTest {
+import static org.junit.Assert.assertEquals;
+import java.lang.reflect.Method;
+import server.ServerController;
+import org.junit.Before;
+import org.junit.Test;
+import game.PlayerColor;
 
-}
+public class ServerControllerTest {
+	
+	    public ServerController testController;
+	    
+	    @Before
+	    public void beforeClass() {
+	        testController = new ServerController();
+	    }
+
+	    @Test
+	    public void unitTest() {
+	        try {	           
+	            Method method = ServerController.class.getDeclaredMethod("decodeColor", String.class);
+	            method.setAccessible(true);
+	            assertEquals(PlayerColor.RED, method.invoke(testController, "red"));
+	        } 
+	        catch (Exception e) {e.printStackTrace();}	        
+	    }
+	}
