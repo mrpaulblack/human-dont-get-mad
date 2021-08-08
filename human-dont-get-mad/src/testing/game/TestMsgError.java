c82ba090b479f5627495ebb46daa650d611fed96
UrsusNix <87434040+UrsusNix@users.noreply.github.com>
2021-08-08 09:56:40 +0200

    Unit test first wave (#9)
    * first test for server.decodeplayercolor
    
    * adding tests for server.ServerController decodeColor
    
    * update to the test of ServerController
    
    * adding test for GamePosition
    
    * adding test for GameState
    
    * added test for MsgError
    
    * adding test for Msg.Type
    
    * added test to player color

---
+++ human-dont-get-mad/src/testing/game/TestMsgError.java
@@ -0,0 +1,68 @@
+package testing.game;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import game.MsgError;
+
+/**
+* <h1>Unit-Test for game.MsgError</h1>
+* <p>This is a unit test of the methods found in the MsgError class within the game package</p>
+
+*
+* @author  Konrad Krueger
+* @version 1.0
+* @since   2021-07-24
+* @apiNote MAEDN 3.0
+* @implNote JUnit 4
+*/
+
+public class TestMsgError 
+{	
+    //Test of the convert toString method for the Unsupported Message Type-case
+	@Test
+	public void testToStringUnsupportedMessageType()	
+	{ 
+		String testString = "unsupportedMessageType";
+		assertEquals(MsgError.UNSUPPORTEDMESSAGETYPE.toString(), testString);	
+	}
+	
+    //Test of the convert toString method for the Unsupported Protocol Version-case
+	@Test
+	public void testToStringUnsupportedProtocolVersion()	
+	{ 
+		String testString = "unsupportedProtocolVersion";
+		assertEquals(MsgError.UNSUPPORTEDPROTOCOLVERSION.toString(), testString);	
+	}
+	
+    //Test of the convert toString method for the Server Full-case
+	@Test
+	public void testToStringServerFull()	
+	{ 
+		String testString = "serverFull";
+		assertEquals(MsgError.SERVERFULL.toString(), testString);	
+	}
+	
+    //Test of the convert toString method for the Illegal Move-case
+	@Test
+	public void testToStringIllegalMove()	
+	{ 
+		String testString = "illegalMove";
+		assertEquals(MsgError.ILLEGALMOVE.toString(), testString);	
+	}
+	
+    //Test of the convert toString method for the Not Your Turn-case
+	@Test
+	public void testToStringNotYourTurn()	
+	{ 
+		String testString = "notYourTurn";
+		assertEquals(MsgError.NOTYOURTURN.toString(), testString);	
+	}
+	
+    //Test of the convert toString method for the Unknown-case
+	@Test
+	public void testToStringUnknown()	
+	{ 
+		String testString = "unknown";
+		assertEquals(MsgError.UNKNOWN.toString(), testString);	
+	}	
+}
